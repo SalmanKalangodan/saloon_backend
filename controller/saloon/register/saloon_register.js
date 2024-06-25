5327f2fe3684a1293724d38e0336223c63a37ede
salman k <salman371k@gmail.com>
2024-06-25 10:02:57 +0530

    .

--- controller/saloon/register/saloon_register.js
+++ controller/saloon/register/saloon_register.js
@@ -1,3 +1,14 @@
 import mongoose from "mongoose";
 
 
+const woekrSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    email : {
+        type : String,
+        required : true
+    },
+    
+})
